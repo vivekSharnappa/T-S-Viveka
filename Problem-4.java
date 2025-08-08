a3d6c398c84f1dc816707576ea35c5cef6ba7c7e
T S Viveka <159865739+vivekSharnappa@users.noreply.github.com>
2025-08-08 16:49:40 +0530

    Update Problem-4.java

--- Problem-4.java
+++ Problem-4.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+public class Problem4 {
+    public static void main(String[] args) {
+        int[] input = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+
+        for (int i = 1; i <= 9; i++) {
+            int count = 0;
+            for (int num : input) {
+                if (num % i == 0) {
+                    count++;
+                }
+            }
+            map.put(i, count);
+        }
+
+        System.out.println(map);
+    }
+}
