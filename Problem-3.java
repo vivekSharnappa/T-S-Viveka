12718088e83dc44b147dd4a2516dcb9cc2f85a02
T S Viveka <159865739+vivekSharnappa@users.noreply.github.com>
2025-08-08 16:38:32 +0530

    Update Problem-3.java

--- Problem-3.java
+++ Problem-3.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Program3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        // If number is even, reduce by 1 to make it odd
+        if (num % 2 == 0) {
+            num = num - 1;
+        }
+
+        // Print the series of odd numbers
+        for (int i = 1; i <= num * 2; i += 2) {
+            System.out.print(i);
+            if (i < num * 2 - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
