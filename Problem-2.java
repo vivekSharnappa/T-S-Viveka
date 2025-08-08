9978a6b6983328300dc4ab772e6139d4f5c270cd
T S Viveka <159865739+vivekSharnappa@users.noreply.github.com>
2025-08-08 16:27:15 +0530

    Update Problem-2.java

--- Problem-2.java
+++ Problem-2.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class Program2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+
+        for (int i = 0; i < x; i++) {
+            System.out.print((2 * i + 1));
+            if (i < x - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
